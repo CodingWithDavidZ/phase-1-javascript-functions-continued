c6ccfea0ce73851292cc004d31c0a92fee967ea2
David Zombar <davidzombar@gmail.com>
2021-06-29 16:28:13 -0600

    commit tree 4bf9db0c2df57be3a86e7fe29a7c83681b75098d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (location = "go to the office") {
+  return `This Monday, I will ${location}.`;
+};
+
+//write function called wrapAdjective
+let wrapAdjective = function (flair = "*") {
+  //write innerFunction that takes single param and defaults to "special"
+  return function (argument = "special") {
+    //return string "You are..." where ... is param
+    return `You are ${flair}${argument}${flair}!`;
+    //it should take a param string that should default to "*"
+  };
+};
